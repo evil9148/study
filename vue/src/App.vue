2ee987fb4dd10a28e4417f1dc22f59d89162c7c2
DESKTOP-UIPUNVV\evil9 <a@a.net>
2023-06-16 02:10:07 +0900

    vue study

---
+++ vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="name">
+    {{ name }}
+  </div>
+  <button class="btn btn-primary" v-on:click="changeName">
+    click
+    </button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const name = ref("gijun coder1");
+
+    //   const greeting = (name) => {
+    //     return "Hello, " + name;
+    //   };
+
+    // const greet = greeting(name)
+    const changeName = ()=>{
+      name.value = "gijun coder"
+    }
+    return {
+      name,
+      changeName,
+    };
+  },
+};
+</script>
+
+<style>
+.name {
+  color: red;
+}
+</style>
